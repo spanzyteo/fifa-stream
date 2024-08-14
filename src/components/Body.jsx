edaba4d6474087e56935adf085d9f3fa6443a67e
spanzy teo <spanzytheo@gmail.com>
2024-08-14 09:18:05 +0100

    modidications added to the page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
-import playStore from '../assets/google-play.jpeg'
+import playStore from '../assets/google-play.png'
 import { Link } from 'react-router-dom'
 import { FaCopy } from 'react-icons/fa'
 import { FaRegCopy } from 'react-icons/fa'
 import skateMan from '../assets/skate.png'
+import { motion } from 'framer-motion'
+import vid from '../assets/video.mp4'
+import vid1 from '../assets/vid.mp4'
 
 const Body = () => {
   const walletAddress = '8EGkaTTM2fdKFrZhk8gWiVWShWTRmBXqcoHxJphJ3qTV'
@@ -34,43 +37,62 @@ const Body = () => {
     }
   }
   return (
-    <div className="mt-20 flex flex-col w-[300px] md:w-[550px] mx-auto">
-      <div className="text-white mx-auto text-2xl font-semibold">
-        <h1 className="flex text-center text-red-700 md:text-3xl uppercase">
-          Watch Live Events From Your Phone
-        </h1>
-      </div>
-      <div className="mt-10 text-white text-center uppercase">
-        The Livestream app grants you mobile access to go live to your events,
-        find us on Solana now.
-      </div>
-      <div className="flex flex-col text-white mt-8">
-        <h1 className="text-center text-3xl font-bold uppercase">Contract</h1>
-        <div className="flex items-center item border border-gray-500 mt-5 px-5 pb-3 rounded-lg mx-auto">
-          <h1 className="mt-5 break-all text-xs sm:text-lg text-center">
-            {walletAddress}
-          </h1>
-          <div className="">
-            <FaRegCopy
-              className="ml-2 cursor-pointer hover:text-gray-700 h-[20px] w-[20px] text-white mt-4"
-              onClick={copyToClipboard}
-              title="Copy Wallet Address"
-            />
-            {/* {isCopied && <span className="ml-2 text-sm text-black">Copied!</span>} */}
+    <div className="overflow-hidden">
+      <div className="mt-0 flex relative flex-col w-[100%] lg:w-[1500px] h-[100%] mx-auto">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute top-0 left-0 lg:ml-[-5rem]  w-full h-full object-cover -z-10 "
+        >
+          <source src={vid} type="video/mp4" />
+        </video>
+        <motion.div
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="mt-20 w-[300px] md:w-[550px] mx-auto"
+        >
+          <div className="mx-auto text-2xl font-semibold">
+            <h1 className="flex text-center text-red-700 md:text-3xl uppercase">
+              Watch Live Events From Your Phone
+            </h1>
           </div>
-        </div>
-      </div>
-      <Link
-        to={
-          'https://play.google.com/store/apps/details?id=com.livestream.livestream'
-        }
-        target="_blank"
-        className="mt-10 mx-auto"
-      >
-        <img src={playStore} alt="" className="h-[60px]" />
-      </Link>
-      <div className="h-[400px] md:h-[600px]">
-        <img src={skateMan} alt="" className="h-full w-full" />
+          <div className="mt-10 text-black font-bold text-center uppercase">
+            The Livestream app grants you mobile access to go live to your
+            events, find us on Solana now.
+          </div>
+          <div className="flex flex-col mt-8">
+            <h1 className="text-center text-3xl font-bold uppercase">
+              Contract
+            </h1>
+            <div className="flex items-center item border border-gray-500 text-white mt-5 px-5 pb-3 rounded-lg mx-auto font-semibold">
+              <h1 className="mt-5 break-all text-xs sm:text-lg text-center">
+                {walletAddress}
+              </h1>
+              <div className="">
+                <FaRegCopy
+                  className="ml-2 cursor-pointer hover:text-gray-700 h-[20px] w-[20px] text-white mt-4"
+                  onClick={copyToClipboard}
+                  title="Copy Wallet Address"
+                />
+                {/* {isCopied && <span className="ml-2 text-sm text-black">Copied!</span>} */}
+              </div>
+            </div>
+          </div>
+          <Link
+            to={
+              'https://play.google.com/store/apps/details?id=com.livestream.livestream'
+            }
+            target="_blank"
+            className="mt-10"
+          >
+            <img src={playStore} alt="" className="h-[170px] mx-auto mt-6" />
+          </Link>
+          <div className="h-[400px] md:h-[600px]">
+            <img src={skateMan} alt="" className="h-full w-full" />
+          </div>
+        </motion.div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import liveStream from '../assets/live-stream.png'
 
 const Navbar = () => {
   return (
-    <div className="h-[80px] flex items-center  border-b border-gray-700">
+    <div className="h-[80px] flex items-center bg-white">
       <img src={logo} alt="" className="h-[50px] md:h-[60px] w-[50px] md:w-[60px] ml-5 md:ml-16" />
-      <div className='flex flex-col uppercase ml-5 text-white'>
+      <div className='flex flex-col uppercase ml-5 text-black'>
         <h1 className='text-2xl md:text-3xl font-bold'>Live</h1>
         <h1 className='text-2xl font-semibold'>Stream</h1>
       </div>
